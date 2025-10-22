9841bb1a189226a316e60ea8fa962156d4fd0ee7
world <world@Worldink.Home>
2025-10-22 11:25:52 +0200

    Third Commit: Aggiunto Insulto

---
+++ src/main/java/lezione_20251022_maven_e_git_oscar/Programma.java
@@ -0,0 +1,12 @@
+package lezione_20251022_maven_e_git_oscar;
+
+public class Programma {
+
+	public static void main(String[] args) {
+		
+		Studente st = new Studente("MAT2011", "Oscar", "Diaz", 25, "Sbirulino");
+		System.out.println(st.toString());
+
+	}
+
+}
