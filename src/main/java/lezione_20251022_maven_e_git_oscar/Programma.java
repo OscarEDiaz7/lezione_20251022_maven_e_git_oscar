020bed4b06ffa544ccece3f35edde1ad77a9041a
Worldink <worldink96@gmail.com>
2025-10-22 11:52:43 +0200

    Fifth Commit: Committer cambiato

--- src/main/java/lezione_20251022_maven_e_git_oscar/Programma.java
+++ src/main/java/lezione_20251022_maven_e_git_oscar/Programma.java
@@ -4,6 +4,8 @@ public class Programma {
 
 	public static void main(String[] args) {
 		
+		//test autore, committer cambiatp
+		
 		Studente st = new Studente("MAT2011", "Oscar", "Diaz", 25, "Sbirulino");
 		System.out.println(st.toString()); 
 
